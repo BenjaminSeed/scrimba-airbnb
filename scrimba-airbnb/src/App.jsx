7e51a3600b3a625d0a832c5d85809c24c440298a
Benjamin <benseed.bs@gmail.com>
2023-06-28 18:04:32 +0100

    added map to App component

--- scrimba-airbnb/src/App.jsx
+++ scrimba-airbnb/src/App.jsx
@@ -2,20 +2,36 @@ import "./App.css";
 import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import data from "./Data";
 
 function App() {
+  const newData = data.map((info) => {
+    return (
+      <Card
+        title={info.title}
+        description={info.description}
+        coverImg={info.coverImg}
+        rating={info.stats.rating}
+        price={info.price}
+        reviewCount={info.stats.reviewCount}
+        location={info.location}
+        openSpots={info.openSpots}
+      />
+    );
+  });
   return (
     <>
       <Navbar />
       <Hero />
-      <Card
+      {newData}
+      {/* <Card
         img={"../src/assets/image 12.png"}
         rating="5.0"
         reviewCount={6}
-        country="USA"
+        location="USA"
         title="Life Lessons with Katie Zaferes"
         price={136}
-      />
+      /> */}
     </>
   );
 }
--- scrimba-airbnb/src/components/Card.jsx
+++ scrimba-airbnb/src/components/Card.jsx
@@ -1,22 +1,30 @@
 import "../styles/Card.css";
-import swimmer from "../assets/image 12.png";
 import star from "../assets/Star 1.png";
 
-const Card = ({ img, rating, reviewCount, country, title, price }) => {
+const Card = ({
+  coverImg,
+  rating,
+  reviewCount,
+  location,
+  title,
+  price,
+  openSpots,
+}) => {
   return (
     <div className="card">
-      <img src={img} className="card--img" />
+      <img src={coverImg} className="card--img" />
       <div className="card--stats">
         <img src={star} alt="" className="card--star" />
         <span>{rating}</span>
         <span className="grey">({reviewCount}) • </span>
-        <span className="grey">{country}</span>
+        <span className="grey">{location}</span>
       </div>
       <p>{title}</p>
       <p>
         {" "}
         <span className="bold">From ${price}</span> / Person
       </p>
+      <p>Available Spaces: {openSpots}</p>
     </div>
   );
 };
